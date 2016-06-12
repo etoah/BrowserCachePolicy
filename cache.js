09a9a0124e0be78dcfa9b793510d45d306ecb99a
Lucien <etoahn@163.com>
2016-06-13 07:36:53 +0800

    cancel comment

--- cache.js
+++ cache.js
@@ -14,7 +14,7 @@ module.exports = function (request, response) {
         expires.setTime(expires.getTime() + config.Expires.maxAge * 1000);
 
         response.setHeader("Expires", expires.toUTCString());
-        //response.setHeader("Cache-Control", "max-age=" + config.Expires.maxAge);
+        response.setHeader("Cache-Control", "max-age=" + config.Expires.maxAge);
 
     }
 }
